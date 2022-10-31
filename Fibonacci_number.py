ebc8ffc23546de064715b3c7a199af215f9785e0
Chousik <78282521+Chousik@users.noreply.github.com>
2022-10-31 16:42:10 +0300

    Create Fibonacci_number.py

---
+++ Fibonacci_number.py
@@ -0,0 +1,15 @@
+import sys
+sys.set_int_max_str_digits(10**6)
+
+
+def fib(n):
+    if n<10:
+        return [1,1,2,3,5,8,13,21,34,55][n]
+    elif n % 2==0:
+        return fib2(n//2)**2+fib2(n//2-1)**2
+    else:
+        return fib2(n//2)*(fib2(n//2+1)+fib2(n//2-1))
+
+
+n = int(input())
+print(fib(n))
